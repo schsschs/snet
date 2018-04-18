e9ee9b4d0c0d49a083a7ddcff26b792a7d1b5645
Sergej Schmidt <sergej@ewlicht.de>
2018-04-18 17:21:34 +0200

    types.h completly and add snet.h Signed-off-by: Sergej Schmidt <sergej@ewlicht.de>

--- snet/snet.h
+++ snet/snet.h
@@ -1,3 +1,49 @@
-#ifndef SNET_H
-#define SNET_H
-#endif // !SNET_H
+#ifndef __SNET_H__
+#define __SNET_H__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#include <stdlib.h>
+
+#ifdef _WIN32
+#include "snet/win32.h"
+#else
+#include "snet/unix.h"
+#endif
+
+#include "snet/types.h"
+#include "snet/protocol.h"
+#include "snet/list.h"
+#include "snet/callbacks.h"
+
+#define SNET_VERSION_MAJOR 0
+#define SNET_VERSION_MINOR 0
+#define SNET_VERSION_PATCH 1
+#define SNET_VERSION_CREATE(major, minor, patch) (((major)<<16) | ((minor)<<8)|(patch))
+#define SNET_VERSION_GET_MAJOR(version) (((version)>>16)&0xFF)
+#define SNET_VERSION_GET_MINOR(version) (((version)>>8)&0xFF)
+#define SNET_VERSION_GET_PATCH(version) ((version)&0xFF)
+#define SNET_VERSION SNET_VERSION_CREATE(SNET_VERSION_MAJOR, SNET_VERSION_MINOR, SNET_VERSION_PATCH)
+
+	typedef snet_uint32 SNetVersion;
+
+	struct _SNetHost;
+	struct _SNetEvent;
+	struct _SNetPacket;
+
+	typedef enum _SNetSocketType
+	{
+		SNET_SOCKET_TYPE_STREAM = 1,
+		SNET_SOCKET_TYPE_DATAGRAM = 2
+	} SNetSocketType;
+
+	// TODO
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // !__SNET_H__
--- snet/types.h
+++ snet/types.h
@@ -1,3 +1,8 @@
 #ifndef __SNET_TYPES_H__
 #define __SNET_TYPES_H__
+
+typedef unsigned char snet_uint8;
+typedef unsigned short snet_uint16;
+typedef unsigned int snet_uint32;
+
 #endif // !__TYPES_H__
